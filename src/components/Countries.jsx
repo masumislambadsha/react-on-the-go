01da36bd832ae46a795d82b63ebd01d0e18554b4
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-09-19 08:19:48 +0600

    first commit

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,15 +1,17 @@
 import React, { use } from 'react';
+import Country from './Country';
 
 const Countries = ({countriesPromise}) => {
 
   const countriesData = use(countriesPromise)
   const countries = countriesData.countries
+  console.log(countriesData)
   console.log(countries)
   return (
     <div>
       <h1>In the countries {countries.length}</h1>
       {
-
+        countries.map(country => <Country country={country}></Country>)
       }
     </div>
   );
