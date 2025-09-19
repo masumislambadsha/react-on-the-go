4d985ca1f091eec73ff51c7269f5c0c45271dcd1
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-09-19 09:47:16 +0600

    first commit

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,9 +1,10 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from './Country';
 import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
 
+  const [,] =useState()
   const countriesData = use(countriesPromise)
   const countries = countriesData.countries
   // console.log(countriesData)
@@ -11,9 +12,12 @@ const Countries = ({countriesPromise}) => {
   return (
     <div>
       <h1>In the countries {countries.length}</h1>
+      <h2>Total countries visited: </h2>
       <div className='countries'>
          {
-        countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+        countries.map(country => <Country key={country.cca3.cca3} country={country}
+
+        ></Country>)
       }
       </div>
 
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import'./country.css'
 
 const Country = ({country}) => {
 
+    const [visited, setVisited] = useState(false)
+
     const handleVisit =() =>{
-      
+      // if(visited){
+      //   setVisited(false)
+      // }
+      // else{
+
+      //   setVisited(true)
+      // }
+      // setVisited(visited? false: true)
+      setVisited(!visited)
     }
   // console.log(country)
   return (
-    <div className='country'>
+    // <div className={`country ${visited? 'country-visited':'country-not-visited'}`}>
+    <div className={`country ${visited &&'country-visited'}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
         <h3>Name: {country.name.common}</h3>
         <p>Population: {country.population.population}</p>
         <p>It's in <b> {country.continents.continents} </b> Continent</p>
        <p>Area : {country.area.area} {country.area.area > 300000? 'Big Country' :'Small Country'}</p>
-       <button onClick={handleVisit}>Not Visited</button>
+       <button onClick={handleVisit}>
+        {visited? 'Visited':'Not Visited'}
+       </button>
     </div>
   );
 };
