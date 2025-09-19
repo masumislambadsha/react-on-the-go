e91ef3f0cbb5560ee0f6dc1f3ac235a377a0be87
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-09-19 08:02:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Countries from './components/Countries'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-bold'>React world on the go</h1>
+      <Countries></Countries>
+      </>
+  )
+}
+
+export default App
