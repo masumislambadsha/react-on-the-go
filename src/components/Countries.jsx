b87b3a6fd8409d5b19a54c800a725263d86b1b2c
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-09-19 09:22:44 +0600

    first commit

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import Country from './Country';
+import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
 
@@ -10,9 +11,12 @@ const Countries = ({countriesPromise}) => {
   return (
     <div>
       <h1>In the countries {countries.length}</h1>
-      {
+      <div className='countries'>
+         {
         countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
       }
+      </div>
+
     </div>
   );
 };
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import'./country.css'
 
 const Country = ({country}) => {
+
+    const handleVisit =() =>{
+      
+    }
   // console.log(country)
   return (
-    <div>
+    <div className='country'>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
         <h3>Name: {country.name.common}</h3>
         <p>Population: {country.population.population}</p>
         <p>It's in <b> {country.continents.continents} </b> Continent</p>
-        <p>Language of here:  </p>
+       <p>Area : {country.area.area} {country.area.area > 300000? 'Big Country' :'Small Country'}</p>
+       <button onClick={handleVisit}>Not Visited</button>
     </div>
   );
 };
