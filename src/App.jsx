b548551f025ed0cb3f4df77dac2c9f3aac70bca6
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-09-19 08:10:37 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import Countries from './components/Countries'
-import './App.css'
+import Countries from "./components/Countries";
+import "./App.css";
+import { Suspense } from "react";
+
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all"
+).then((res) => res.json());
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-bold'>React world on the go</h1>
-      <Countries></Countries>
-      </>
-  )
+      <h1 className="text-bold">React world on the go</h1>
+
+      <Suspense fallback={<p>Nadir Bhai Loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
+    </>
+  );
 }
 
-export default App
+export default App;
