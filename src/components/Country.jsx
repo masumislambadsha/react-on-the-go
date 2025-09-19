6c60a12dd11f8e8dfd34d8b0bab9875d15c805d2
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-09-19 08:37:53 +0600

    first commit

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const Country = ({country}) => {
-  console.log()
+  // console.log(country)
   return (
     <div>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt}/>
         <h3>Name: {country.name.common}</h3>
         <p>Population: {country.population.population}</p>
+        <p>It's in <b> {country.continents.continents} </b> Continent</p>
+        <p>Language of here:  </p>
     </div>
   );
 };
